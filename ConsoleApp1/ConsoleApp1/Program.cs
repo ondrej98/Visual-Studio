e2d3ce82209188eb3028a4cfc0df873e6544c401
Michal Kováč <xkovacm12@stuba.sk>
2019-10-29 16:06:37 +0100

    Nove

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,13 +13,13 @@ namespace ConsoleApp1
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
             string citanie = Console.ReadLine();
             string format = string.Format("{0}:{1}", "Výsledok", citanie);
             
             var poleStringov = format.Split(':')[0];
 
-            Console.WriteLine(poleStringov);*/
+            Console.WriteLine(poleStringov);
             
             string[,] data_pole = new string[,] { { "Ahoj", "Mám sa dobre" } ,{ "Ok", "Povedzme"}, { "Col", "Los"} };
             string[] dt = new string[5];
