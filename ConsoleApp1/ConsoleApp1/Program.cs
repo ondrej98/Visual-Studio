5a857f52a469b871ed7165a6834d6754f3a79da7
Ondrej Durmis <Ondrej Durmis@NB-HP250G7>
2019-10-29 15:58:17 +0100

    Prvé intro

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,38 @@
 
 namespace ConsoleApp1
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Console.WriteLine("Hello World!");
+            string citanie = Console.ReadLine();
+            string format = string.Format("{0}:{1}", "Výsledok", citanie);
+            
+            var poleStringov = format.Split(':')[0];
+
+            Console.WriteLine(poleStringov);*/
+            
+            string[,] data_pole = new string[,] { { "Ahoj", "Mám sa dobre" } ,{ "Ok", "Povedzme"}, { "Col", "Los"} };
+            string[] dt = new string[5];
+            for(int i= 0; i<3; i++)
+            {
+                if (i < 2)
+                    Console.WriteLine(data_pole.GetLength(i));
+                else
+                    Console.WriteLine(data_pole.Length);
+            }
+
+
+
+
         }
     }
 }
