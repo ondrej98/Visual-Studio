bcfa7e6a5e145f6a02eda7dbb34adf388b222dd4
Ondrej Durmis <Ondrej Durmis@NB-HP250G7>
2019-10-30 07:57:28 +0100

    Vytvorenie banche

---
+++ WindowsFormsApp1/WindowsFormsApp1/ClassMoje.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class ClassMoje
+    {
+        private int _id;
+
+        public int ID { get { return _id; } private set { _id = value; OnIDChange(value); } }
+
+        public EventHandler IDChanged;
+
+        protected virtual void OnIDChange(object sender)
+        {
+            IDChanged?.Invoke(sender, EventArgs.Empty);
+        }
+
+        public ClassMoje()
+        {
+
+        }
+    }
+}
